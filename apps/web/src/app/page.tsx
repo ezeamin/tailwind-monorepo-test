319f9a0ba34a7c6354da8cb6c3dc453edcfc03eb
Ezequiel Amin <ezequielamin@outlook.com>
2023-09-26 10:05:23 -0300

    hola

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <main className="bg-slate-200">
+      <button className="text-red-200">hola</button>
+      <h1 className="text-blue-400">chau</h1>
+    </main>
+  );
+}
+
+export default Home
